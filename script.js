82fd0ec483a999c6958bd2cb9708cb24e289145a
unknown <pv95335@gmail.com>
2020-08-09 22:57:33 -0700

    Databases working

--- script.js
+++ script.js
@@ -1,13 +1,19 @@
-const mysql = require("mysql2/promise");
+const mysql = require("mysql2");
 const cTable = require("console.table");
 const inquirer = require("inquirer");
 
-const connection = await mysql.createConnection({
+const connection = mysql.createConnection({
   host: "localhost",
   port: 3306,
   user: "root",
   password: "pv95335976!",
   database: "con_manage",
+});
+
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Connected as id" + connection.threadId + "\n");
+  startProject();
 });
 
 const startProject = () => {
@@ -31,78 +37,22 @@ const options = [
   "Update employee manager",
 ];
 
-// addEmployee = () => {
-//   console.log("Adding new employee...\n");
-//   const query = connection.query(
-//     "INSERT INTO id SET ?",
-//     {
-//       employeefirstname: "Jak",
-//       employelastname: "Bridges",
-//       employeetitle: "Head Chef",
-//       department: "Kitchen",
-//       salary: "42,000",
-//       manager: "Sherlock Holms",
-//     },
-//     function (err, res) {
-//       if (err) throw err;
-//       console.log(res.affectedRows + "employee added!\n");
-//       updateRoster();
-//     }
-//   );
-//   console.log(query.sql);
-// };
+addEmployee = () => {
+  console.log("Adding new employee...\n");
+};
 
-// updateEmployeeInfo = () => {
-//   console.log("Updating database...\n");
-//   const query = connection.query(
-//     [
-//       {
-//         salary: 500,
-//       },
-//       {
-//         department: "closet",
-//       },
-//     ],
-//     function (err, res) {
-//       if (err) throw err;
-//       console.table(`${res.affectedRows} updated info!\n`);
-//       deleteInfo();
-//     }
-//   );
-//   console.log(query.sql);
-// };
+updateEmployeeInfo = () => {};
 
-// deleteEmployee = () => {
-//     console.log("Deleteing information...\n");
-//     const query = connection.query(
-//         "DELETE FROM employeeinfo WHERE ?",
-//         function (err, res) {
-//             if(err) throw err;
-//             console.table(res.affectedRows + 'Employee removed!');
-//             readInfo();
-//         }
-//     ),
-// };
+deleteEmployee = () => {};
 
-// readInfo = () => {
-//     console.log("Selecting all info ... \n");
-//     const query = connection.query(
-//         "SELECT * FROM employeeinfo",
+readInfo = () => {};
 
-//         function (err, res) {
-//             if(err) throw err;
-//             console.table(res);
-//             connection.end();
-//         }
-//     )
-// }
+connection.query(
+  'SELECT * FROM `id` WHERE `employeeinfo` = "employeefirst" AND `Max`',
+  function (err, results, fields) {
+    console.log(results);
+    console.log(fields);
+  }
+);
 
-// connection.query(
-//   'SELECT * FROM `id` WHERE `employeeinfo` = "employeefirst" AND `Max`',
-//   function (err, results, fields) {
-//     console.log(results);
-//     console.log(fields);
-//   }
-// );
-
-startProject();
+// startProject();
